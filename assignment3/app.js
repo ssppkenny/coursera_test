48bbc41d58aee92efd9eaf802ddaf4abb5701f3e
Sergey Mikhno - MySign AG <sergey@mysigndomain.corp>
2016-11-24 15:39:42 +0100

    directive

--- assignment3/app.js
+++ assignment3/app.js
@@ -1,7 +1,9 @@
 (function() {
   'use strict';
   angular.module('NarrowItDownApp', []).controller('NarrowItDownController', NarrowItDownController)
-  .service('MenuSearchService', MenuSearchService);
+  .service('MenuSearchService', MenuSearchService).directive('foundItems', FoundItems);
+
+  // .directive('foundItems', FoundItems)
 
   NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
 
@@ -9,11 +11,14 @@
     var ctrl = this;
     ctrl.inputtext = "";
     ctrl.getMatchedMenuItems = function(searchTerm) {
+      var promise = MenuSearchService.getMatchedMenuItems(ctrl.inputtext);
+      promise.then(function(response) {
+        ctrl.found = response;
+      });
 
-      return MenuSearchService.getMatchedMenuItems(ctrl.inputtext);
     }
-
   }
+
 
   MenuSearchService.inject = ['$scope', '$http'];
 
@@ -21,22 +26,35 @@
      var service = this;
 
      service.getMatchedMenuItems = function(searchTerm) {
-
        var promise = $http({
          method: 'GET',
          url: 'http://davids-restaurant.herokuapp.com/menu_items.json'
        });
-
-       promise.then(function(response) {
+       var found = null;
+       return promise.then(function(response) {
          var data = response.data;
          var menu_items = data.menu_items;
 
-         var found = menu_items.filter(function( item ) {
+         found = menu_items.filter(function( item ) {
            return item.description.indexOf(searchTerm) > -1;
          });
+         return found;
        });
+
      }
   }
+
+
+    function FoundItems() {
+      var ddo = {
+        templateUrl: 'foundItems.html',
+        restrict: 'E',
+        scope: {
+          list: '=myList'
+        }
+      };
+      return ddo;
+    }
 
 
 
